591e46355fdb0f5e8c5079604d2f2bfd47f3b6cd
LAPTOP-GTQCMDG9\Asus <vidyaratmayanti@gmail.com>
2023-10-19 13:58:23 +0700

    menambahkan implementasi CI

--- Folder tanpa ci/nav.php
+++ Folder tanpa ci/nav.php
@@ -1,23 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-<<<<<<< HEAD
-    <title>Log In</title>
-=======
-    <title>Login</title>
->>>>>>> ce7a91a8df3335789fdd60d1b4f12c764c384535
-    <link
+<link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
       rel="stylesheet"
       integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
       crossorigin="anonymous"
     />
-    <link rel="stylesheet" href="styleLogin.css" />
-  </head>
-  <body>
-    <nav class="navbar navbar-expand-lg position-fixed w-100">
+    <link rel="stylesheet" href="styleUtama.css" />
+<nav class="navbar navbar-expand-lg position-fixed w-100">
       <div class="container">
         <a class="navbar-brand" href="#">
           <img src="relawain logo 1.png" alt="" />
@@ -48,22 +36,12 @@
               <a class="nav-link" href="#">Berita</a>
             </li>
           </ul>
+          <div>
+            <button class="login">Masuk</button>
+          </div>
+          <div>
+            <button class="daftar">Daftar</button>
+          </div>
         </div>
       </div>
     </nav>
-    <div class="containerClass" align="center">
-      <form width="50px" height="50px" align ="center">
-          <h1 text-align="center">Log In</h1>
-          <input type="text" class="form-control" name="username" id="username" placeholder="Username" value=""><br>
-          <input type="password" class="form-control" name="password" id="password" placeholder="Password" value=""><br>
-          <button class="buttonLog">Log in</button>
-      </form>
-  </div>
-    <!-- Footer -->
-    <script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-      crossorigin="anonymous"
-    ></script>
-  </body>
-</html>
---
+++ Klp.WebProCi/application/views/nav.php
@@ -0,0 +1,47 @@
+<link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="styleUtama.css" />
+<nav class="navbar navbar-expand-lg position-fixed w-100">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          <img src="relawain logo 1.png" alt="" />
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav mx-auto">
+            <li class="nav-item mx-4">
+              <a class="nav-link active" aria-current="page" href="#">Utama</a>
+            </li>
+            <li class="nav-item mx-4">
+              <a class="nav-link" href="#">Tentang</a>
+            </li>
+            <li class="nav-item mx-4">
+              <a class="nav-link" href="#">Fitur</a>
+            </li>
+            <li class="nav-item mx-4">
+              <a class="nav-link" href="#">Berita</a>
+            </li>
+          </ul>
+          <div>
+            <button class="login">Masuk</button>
+          </div>
+          <div>
+            <button class="daftar">Daftar</button>
+          </div>
+        </div>
+      </div>
+    </nav>
