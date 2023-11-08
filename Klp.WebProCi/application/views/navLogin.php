fbd7985b3afdd7d5d0216998e8a57534f4d53e49
fadelalif <fadelkarut@gmail.com>
2023-11-09 02:49:42 +0700

    perbaiki nav

--- Klp.WebProCi/application/views/navLogin.php
+++ Klp.WebProCi/application/views/navLogin.php
@@ -57,7 +57,7 @@
             <img src="<?= base_url() ?>assets/img/icon profile.png" alt="" style="width: 67px;" />
           </button>
         </a>
-        <ul class="dropdown-menu" style="background-color: #00897b;">
+        <ul class="dropdown-menu navDropdownMenuLink" style="background-color: #00897b;">
           <li><a class="dropdown-item profil" href="<?= base_url() ?>index.php/Profile">Profil</a></li>
           <li><a class="dropdown-item keluar" href="<?= base_url() ?>index.php/Utama">Keluar</a></li>
         </ul>
