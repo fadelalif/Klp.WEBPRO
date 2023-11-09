c88c9687b79f96d41587a368b488a00ad5c883c6
Vanillateaa <mohamad.william24@gmail.com>
2023-11-09 10:42:22 +0700

    tentang kami 091123

---
+++ Klp.WebProCi/application/views/page/tentangkami.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="StyleTenteangkami.css">
+</head>
+<body>
+<?php include("nav.php");
+?>
+<div class="container-fluid"> 
+  <div class=" text-center tentang-kami header-background">
+    <h1 class="mb-5" style="color: white; margin-top: 50px; font-size: 120px; font-weight: 1500; "> TENTANG KAMI</h1> 
+    <p style="color: white ; font-size: 25px; text-align: center;">Aplikasi Relawanin menyediakan akses mudah dan cepat kepada sukarelawan.  <br>Para pengguna dapat melihat aktivitas yang tersedia, <br> melihat deskripsi, lokasi, dan persyaratan yang diperlukan sehingga memudahkan mereka <br>untuk menemukan proyek yang sesuai dengan minat dan keahlian mereka.</p>
+  </div>
+  
+  <div class=" text-center">
+    <div class="kotak mx-auto" style="position: relative; top: 50px; border-radius: 20px;">
+        <div class="row align-items-start">
+            <div class="col" style="margin-top: 90px; margin-bottom: 90px;">
+              <img src="rame.jpg" alt="" class="gambar-gunung" width="500" style="border-radius: 20px;">
+            </div>
+            <div class="Tulisan col justify-text ">
+              <h4 style="margin-bottom: 20px; font-size: 40px;;">HI!</h4>
+              <h5 style="margin-bottom: 20px; font-size: 30px; ">Kenalin Kami RelawanIn</h5>
+              <p style="margin-left: 10px; margin-right: 60px; font-size: 20px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+          </div>
+    </div>
+    <div class=" Bawahnya-kotak" style="margin-top: 150px; border-radius: 20px; ">
+        <h2 style="font-size: 100px; color: black; margin-bottom: 150px;">Our <span style="color: #00897b;">Value</span></h2>
+        <div class="card-group " style="margin-right: 200px; margin-left: 200px; font-size: 20px; ">
+            <div class="card">
+              <img src="ijo.jpg" class="card-img-top"  >
+              <div class="card-body">
+                <p class="card-text">Kami mengutamakan kepercayaan dalam setiap interaksi di platform kami. Kami melindungi informasi pribadi relawan dan organisasi dengan keamanan yang kuat serta mempromosikan transparansi dalam hubungan antara kedua belah pihak.
+                </p>             
+              </div>
+            </div>
+            <div class="card">
+              <img src="ijo.jpg" class="card-img-top" >
+              <div class="card-body">
+                <p class="card-text">Kami percaya bahwa setiap aksi sukarela memiliki potensi untuk menciptakan dampak yang signifikan dalam masyarakat. Oleh karena itu, kami berkomitmen untuk mempromosikan proyek-proyek yang bermanfaat dan memberikan kesempatan bagi para relawan untuk berkontribusi dalam pengurangan emisi gas ini.
+                </p>
+              </div>
+            </div>
+            <div class="card">
+              <img src="ijo.jpg" class="card-img-top" >
+              <div class="card-body">
+                <p class="card-text">Kami mendorong partisipasi aktif dari semua pengguna kami. Kami menyediakan pengalaman yang menyenangkan dan bermakna bagi para relawan dan organisasi untuk terlibat dalam kegiatan sukarela.</p>
+              </p>
+              </div>
+            </div>
+        </div>
+    </div>
+  
+    <div class="kotak-visimisi mx-auto" style="margin-top: -500px; margin-right: 200px; margin-left: 200px; font-size: 25px;">
+      <div class="kotak-visidalam mx-auto" style="position: relative; top: 50px; border-radius: 20px;">
+        <h2 style="font-size: 100px; color: black; margin-bottom: 90px; position: relative; top: 70px">Visi <span style="color:#00897b;">Misi</span></h2>
+        <div class="container" style="margin-top: 110px;">
+            <div class="row align-items-start">
+              <div class="col">
+                <h1 style="font-weight: bolder; font-size: 70px; color:#00897b;">01</h1>
+                <h3 style="font-weight: bolder; font-size: 40px;">Visi</h3>
+                <p>Menjadi terdepan sebagai kaum intelektual Pemuda Pancasila dalam meningkatkan kualitas sumber daya manusia dan mengimplementasikan nilai–nilai Pancasila dengan semangat kebersamaan dan persaudaraan demi esensi dan eksistensi SAPMA Pemuda Pancasila di Kota Surakarta pada khususnya dan Indonesia pada umumnya.</p>
+              </div>
+              <div class="col">
+                
+              </div>
+            </div>
+          </div> 
+         
+          <div class="container text-center">
+            <div class="row align-items-center">
+              <div class="col">
+                
+              </div>
+              <div class="col">
+                <h1 style="font-weight: bolder; font-size: 70px; color: #00897b;">02</h1>
+                <h3 style="font-weight: bolder; font-size: 40px;">Misi</h3>
+                <p>1) Menggunakan segala kemampuan dan potensi diri sebagai kaum intelektual untuk memberikan yang terbaik bagi SAPMA Pemuda Pancasila, bangsa dan Negara <br>
+                  2) Pemahaman mendalam dan implementasi nilai–nilai yang terkandung dalam Pancasila (sebagai ideologi kehidupan berbangsa dan bernegara) untuk diri sendiri maupun orang lain dengan atau melalui lembaga SAPMA Pemuda Pancasila.</p>
+              </div>
+            </div>
+          </div> 
+      </div>
+    </div>
+  
+    <div class="Donasi" style="margin-top: 650px; margin-bottom: 100px;">
+        <div class="kotak mx-auto" style="position: relative; top: 50px;">
+            <div class="row align-items-start">
+                <div class="col" style="margin-top: 90px; margin-bottom: 90px;">
+                  <img src="rame.jpg" alt="" class="gambar-gunung" width="500">
+                </div>
+                <div class="Tulisan col justify-text">
+                  <h1 style="margin-bottom: 20px; font-size: 40px;  color: #00897b;">Donasi</h1>
+                    <div class="donasi">
+                      <i class="fa-solid fa-circle-dollar-to-slot fa-5x" style="color: #00897b; padding-top: 40px;"></i>
+                      <p style="padding-top: 30px;">Bantu kami melalui Donasi! <br>setiap donasi yang kalian berikan <br> sangat berarti bagi kami </p>
+                      <p style="font-weight: bolder ; color: #00897b; ">Donasi</p>
+                   </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="kotak-kontak mx-auto" style="margin-top: -600px; margin-right: 200px; margin-left: 200px;">
+        <h1 style="position: relative; top: 80px; font-size: 100px;">Contact<span style="color: #00897b;"> us</span></h1>
+        <div class="email">
+            <i class="far fa-envelope fa-5x" style="color: #00897b; padding-top: 50px;"></i>
+            <p style="padding-top: 30px;">alamatemailkami@gmail.com</p>
+            <p style="font-weight: bolder; color: #00897b;">email</p>
+        </div>
+        <div class="whatsapp text-center">
+            <i class="fab fa-whatsapp fa-5x" style="color: #00897b; padding-top: 50px;"></i>
+            <p style="padding-top: 30px;">081234567891</p>
+            <p style="font-weight: bolder; color: #00897b;">Chat via whatsapp</p>
+        </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
+<!-- 
+<div class="container-fluid text-center tentang-kami header-background">
+  <h1 class="mb-5" style="color: white; margin-top: 50px; font-size: 120px; font-weight: 1500; "> TENTANG KAMI</h1> 
+  <p style="color: white ; font-size: 25px; text-align: center;">Aplikasi Relawanin menyediakan akses mudah dan cepat kepada sukarelawan.  <br>Para pengguna dapat melihat aktivitas yang tersedia, <br> melihat deskripsi, lokasi, dan persyaratan yang diperlukan sehingga memudahkan mereka <br>untuk menemukan proyek yang sesuai dengan minat dan keahlian mereka.</p>
+</div>
+
+<div class="container-fluid text-center">
+  <div class="kotak mx-auto" style="position: relative; top: 50px; border-radius: 20px;">
+      <div class="row align-items-start">
+          <div class="col" style="margin-top: 90px; margin-bottom: 90px;">
+            <img src="rame.jpg" alt="" class="gambar-gunung" width="500" style="border-radius: 20px;">
+          </div>
+          <div class="Tulisan col justify-text ">
+            <h4 style="margin-bottom: 20px; font-size: 40px;;">HI!</h4>
+            <h5 style="margin-bottom: 20px; font-size: 30px; ">Kenalin Kami RelawanIn</h5>
+            <p style="margin-left: 10px; margin-right: 60px; font-size: 20px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+        </div>
+  </div>
+  <div class="Bawahnya-kotak" style="margin-top: 150px; border-radius: 20px; ">
+      <h2 style="font-size: 100px; color: black; margin-bottom: 150px;">Our <span style="color: #00897b;">Value</span></h2>
+      <div class="card-group " style="margin-right: 200px; margin-left: 200px; font-size: 20px; ">
+          <div class="card">
+            <img src="ijo.jpg" class="card-img-top" alt="..." >
+            <div class="card-body">
+              <p class="card-text">Kami mengutamakan kepercayaan dalam setiap interaksi di platform kami. Kami melindungi informasi pribadi relawan dan organisasi dengan keamanan yang kuat serta mempromosikan transparansi dalam hubungan antara kedua belah pihak.
+              </p>             
+            </div>
+          </div>
+          <div class="card">
+            <img src="ijo.jpg" class="card-img-top " alt="...">
+            <div class="card-body">
+              <p class="card-text">Kami percaya bahwa setiap aksi sukarela memiliki potensi untuk menciptakan dampak yang signifikan dalam masyarakat. Oleh karena itu, kami berkomitmen untuk mempromosikan proyek-proyek yang bermanfaat dan memberikan kesempatan bagi para relawan untuk berkontribusi dalam pengurangan emisi gas ini.
+              </p>
+            </div>
+          </div>
+          <div class="card">
+            <img src="ijo.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text">Kami mendorong partisipasi aktif dari semua pengguna kami. Kami menyediakan pengalaman yang menyenangkan dan bermakna bagi para relawan dan organisasi untuk terlibat dalam kegiatan sukarela.</p>
+            </p>
+            </div>
+          </div>
+      </div>
+  </div>
+
+  <div class="kotak-visimisi mx-auto" style="margin-top: -500px; margin-right: 200px; margin-left: 200px; font-size: 25px;">
+    <div class="kotak-visidalam mx-auto" style="position: relative; top: 50px; border-radius: 20px;">
+      <h2 style="font-size: 100px; color: black; margin-bottom: 90px; position: relative; top: 70px">Visi <span style="color:#00897b;">Misi</span></h2>
+      <div class="container" style="margin-top: 110px;">
+          <div class="row align-items-start">
+            <div class="col">
+              <h1 style="font-weight: bolder; font-size: 70px; color:#00897b;">01</h1>
+              <h3 style="font-weight: bolder; font-size: 40px;">Visi</h3>
+              <p>Menjadi terdepan sebagai kaum intelektual Pemuda Pancasila dalam meningkatkan kualitas sumber daya manusia dan mengimplementasikan nilai–nilai Pancasila dengan semangat kebersamaan dan persaudaraan demi esensi dan eksistensi SAPMA Pemuda Pancasila di Kota Surakarta pada khususnya dan Indonesia pada umumnya.</p>
+            </div>
+            <div class="col">
+              
+            </div>
+          </div>
+        </div> 
+       
+        <div class="container text-center">
+          <div class="row align-items-center">
+            <div class="col">
+              
+            </div>
+            <div class="col">
+              <h1 style="font-weight: bolder; font-size: 70px; color: #00897b;">02</h1>
+              <h3 style="font-weight: bolder; font-size: 40px;">Misi</h3>
+              <p>1) Menggunakan segala kemampuan dan potensi diri sebagai kaum intelektual untuk memberikan yang terbaik bagi SAPMA Pemuda Pancasila, bangsa dan Negara <br>
+                2) Pemahaman mendalam dan implementasi nilai–nilai yang terkandung dalam Pancasila (sebagai ideologi kehidupan berbangsa dan bernegara) untuk diri sendiri maupun orang lain dengan atau melalui lembaga SAPMA Pemuda Pancasila.</p>
+            </div>
+          </div>
+        </div> 
+    </div>
+  </div>
+
+  <div class="Donasi" style="margin-top: 700px; margin-bottom: 130px;">
+      <div class="kotak mx-auto" style="position: relative; top: 50px;">
+          <div class="row align-items-start">
+              <div class="col" style="margin-top: 90px; margin-bottom: 90px;">
+                <img src="kevin-zhang-dzFB8xeWg1M-unsplash.jpg" alt="" class="gambar-gunung" width="500">
+              </div>
+              <div class="Tulisan col justify-text">
+                <h1 style="margin-bottom: 20px; font-size: 40px;  color: #00897b;">Donasi</h1>
+                  <div class="donasi">
+                    <i class="far fa-envelope fa-5x" style="color: #00897b; padding-top: 60px;"></i>
+                    <p style="padding-top: 30px;">donasi melalui kita bisa </p>
+                    <p style="font-weight: bolder ; color: #00897b; ">Donasi</p>
+                 </div>
+              </div>
+          </div>
+      </div>
+  </div>
+  <div class="kotak-kontak mx-auto" style="margin-top: -500px; margin-right: 200px; margin-left: 200px;">
+      <h1 style="position: relative; top: 80px; font-size: 100px;">Contact<span style="color: #00897b;"> us</span></h1>
+      <div class="email">
+          <i class="far fa-envelope fa-5x" style="color: #00897b; padding-top: 50px;"></i>
+          <p style="padding-top: 30px;">alamatemailkami@gmail.com</p>
+          <p style="font-weight: bolder; color: #00897b;">email</p>
+      </div>
+      <div class="whatsapp text-center">
+          <i class="fab fa-whatsapp fa-5x" style="color: #00897b; padding-top: 50px;"></i>
+          <p style="padding-top: 30px;">081234567891</p>
+          <p style="font-weight: bolder; color: #00897b;">Chat via whatsapp</p>
+      </div>
+  </div>
+</div> --> 
