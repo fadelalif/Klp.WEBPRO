f3ed1dc0079a7107f1f446bc946cf944bae471d3
fadelalif <fadelkarut@gmail.com>
2023-11-09 23:43:55 +0700

    merge manual branch risky 1/2 #stillclash

--- Klp.WebProCi/application/views/page/cariBerita.php
+++ Klp.WebProCi/application/views/page/cariBerita.php
@@ -15,6 +15,7 @@
 
 <body>
     <div>
+        <?php include 'application/views/navLogin.php'; ?>
     </div>
 
     <!-- Header gambar -->
@@ -103,46 +104,7 @@
     </div>
 
     <!-- footer -->
-    <footer id="footer" class="text-center text-lg-start">
-        <section class="container-fluid p-1 text-end" id="contact-us">
-            <div class="container-fluid text-center text-md-start mt-3  ">
-                <div class="row md-3 justify-content-end">
-                    <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mb-4">
-                        <a>
-                            <img src="<?= base_url() ?>assets/img/icon logo 1.png" alt=""
-                                style="width: 241px; margin-left:56px;" />
-                        </a>
-                    </div>
-                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-4">
-                        <h6 class="text-uppercase fw-bold mb-4" style="color: #00897B;">Layanan Kami</h6>
-                        <p class="footer-links">
-                            <a class="a-footer" href="#" style="text-decoration: none;">Daftarkan Komunitas</a><br>
-                            <a class="a-footer" href="#" style="text-decoration: none;">Tentang Kami</a><br>
-                            <a class="a-footer" href="#" style="text-decoration: none;">Pusat Bantuan</a><br>
-                            <a class="a-footer" href="#" style="text-decoration: none;">Kerjasama</a><br>
-                            <a class="a-footer" href="#" style="text-decoration: none;">Donasi</a><br>
-                        </p>
-                    </div>
-                    <div class="col-md-4 col-lg-4 col-xl-3 mx-auto mb-md-0 mb-4 ms-auto" style="text-align:left;">
-                        <h6 class="text-uppercase fw-bold mb-4" style="color: #fff;"> Informasi Kontak</h6>
-                        <p style="color:#fff"><i class="fa-brands fa-instagram me-3" style="color: #fff;"></i> Relawanin
-                        </p>
-                        <p style="color:#fff"><i class="fas fa-home me-3" style="color: #fff;"></i> Jalan telekomunikasi
-                        </p>
-                        <p style="color:#fff">
-                            <i class="fas fa-envelope me-3 " style="color: #fff;"></i> relawain@gmail.com
-                        </p>
-                        <p style="color:#fff">
-                            <i class="fas fa-phone me-3 " style="color: #fff;"></i> +62 812-3989-290
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <div class="text-center p-3 text-white" style="background-color: #00897B;">
-            © 2023 Copyright Relawanin
-        </div>
-    </footer>
+    <?php include 'application/views/footer.php'; ?>
     <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
     <script>
         AOS.init();
